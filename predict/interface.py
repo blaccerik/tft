d695acda6bad086d2bd43d4a5778194037ef862a
ekalle <ekalle@ttu.ee>
2021-07-05 00:08:33 +0300

    interface

--- predict/interface.py
+++ predict/interface.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
 
-
 class App:
     def __init__(self, root):
         # configure the grid
@@ -10,70 +9,117 @@ class App:
         self.root = root
         # self.sb.grid(column=0, row=0)
 
-        frame = tk.Frame(root)
+        frame = tk.Frame(root, bg="green")
         frame.grid(column=0, row=0)
 
+        self.canvas = tk.Canvas(frame, bg="yellow", width=100)
+        # self.canvas2.grid(row=0, column=0)
 
-        # canvas = tk.Canvas(frame)
-        # canvas.grid(row=0, column=0, sticky="news")
+        self.canvas.pack(side=tk.LEFT)
         #
-        # # Link a scrollbar to the canvas
-        # vsb = tk.Scrollbar(frame, orient="vertical", command=canvas.yview)
-        # vsb.grid(row=0, column=1, sticky='ns')
-        # canvas.configure(yscrollcommand=vsb.set)
+        vsb = tk.Scrollbar(frame, command=self.canvas.yview)
+        vsb.pack(side=tk.LEFT, fill="y")
 
+        # frame3 = tk.Frame(self.canvas2, width=40, height=20)
 
-        text = tk.Text(frame, width=40, height=20)
-        text.grid(column=0, row=0)
-
-        vsb = tk.Scrollbar(frame, orient="vertical", command=text.yview)
-        text.configure(yscrollcommand=vsb.set)
-        vsb.grid(row=0, column=1, sticky='ns')
-
-        # tk.Scrollbar(text, orient="vertical")
-        # text.pack(side="top", fill="both", expand=True)
-        self.chkbuttons = [tk.Checkbutton(text="checkbutton %s" % i, padx=0, pady=0, bd=0)
-                           for i in range(49)]
-        for cb in self.chkbuttons:
-            text.window_create("end", window=cb)
-            text.insert("end", "\n")
-            cb.bind("<Button-1>", self.selectstart)
-            # cb.bind("<Shift-Button-1>", self.selectrange)
-
-        # root.geometry("240x100")
-        # root.title('Login')
-        # root.resizable(0, 0)
-        # root.columnconfigure(0, weight=1)
-        # root.columnconfigure(1, weight=5)
-
-        # username
-        # username_label = ttk.Label(root, text="Username:")
-
-        # self.CheckVar = tk.IntVar(value=0)
+        self.canvas.configure(yscrollcommand=vsb.set)
+        self.canvas.bind('<Configure>', self.configure_left)
         #
-        # username_label = ttk.Checkbutton(root, text="checkbutton", variable=self.CheckVar, command=self.click_me)
-        # username_label.grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
-        #
-        # username_entry = ttk.Entry(root)
-        # username_entry.grid(column=1, row=0, sticky=tk.E, padx=5, pady=5)
-        #
-        # # password
-        # password_label = ttk.Label(root, text="Password:")
-        # password_label.grid(column=0, row=1, sticky=tk.W, padx=5, pady=5)
-        #
-        # password_entry = ttk.Entry(root, show="*")
-        # password_entry.grid(column=1, row=1, sticky=tk.E, padx=5, pady=5)
-        #
-        # # login button
-        login_button = ttk.Button(root, text="Results", command=self.click_me)
-        login_button.grid(column=0, row=1, sticky=tk.W, padx=5, pady=5)
+        frame2 = tk.Frame(self.canvas)
+        self.canvas.create_window((0, 0), window=frame2, anchor='nw')
 
-    def click_me(self):
-        print("aaa")
+        self.champ_list = []
+        self.champ_list_values = []
+        for i in range(30):
+            info = tk.Label(frame2, text="text")
+            info.grid(column=0, row=i)
+            off = tk.IntVar()
+            off.set(1)
+            label = tk.Checkbutton(frame2, variable=off)
+            label.bind("<Button-1>", self.click_champ)
+            label.grid(column=1, row=i)
+            self.champ_list.append(label)
+            self.champ_list_values.append(False)
 
-    def selectstart(self, event):
-        self.start = self.chkbuttons.index(event.widget)
-        print(event)
+
+        frame2 = tk.Frame(root)
+        frame2.grid(row=0, column=1)
+
+        self.canvas2 = tk.Canvas(frame2, bg="yellow", width=100)
+        # self.canvas2.grid(row=0, column=0)
+
+        self.canvas2.pack(side=tk.LEFT)
+        #
+        vsb2 = tk.Scrollbar(frame2, command=self.canvas2.yview)
+        vsb2.pack(side=tk.LEFT, fill="y")
+
+        # frame3 = tk.Frame(self.canvas2, width=40, height=20)
+
+        self.canvas2.configure(yscrollcommand=vsb2.set)
+        self.canvas2.bind('<Configure>', self.configure_right)
+        #
+        frame3 = tk.Frame(self.canvas2)
+        self.canvas2.create_window((0,0), window=frame3, anchor='nw')
+
+        self.item_list = []
+        self.item_list_values = []
+        for i in range(30):
+            # info = tk.Label(frame3, text="text")
+            # info.grid(column=0, row=i)
+
+            label = tk.Label(frame3, text="text: 0")
+            label.grid(column=0, row=i)
+            # label.pack()
+            label.bind("<Button-1>", self.right_click_item)
+            label.bind("<Button-3>", self.left_click_item)
+            self.item_list.append(label)
+            self.item_list_values.append(0)
+
+        res = tk.Button(root, text="Res", command=self.res)
+        res.grid(row=1, column=0)
+
+    def configure_right(self, event):
+        # update scrollregion after starting 'mainloop'
+        # when all widgets are in canvas
+        self.canvas2.configure(scrollregion=self.canvas2.bbox('all'))
+
+    def configure_left(self, event):
+        # update scrollregion after starting 'mainloop'
+        # when all widgets are in canvas
+        self.canvas.configure(scrollregion=self.canvas.bbox('all'))
+
+    def click_champ(self, event):
+        a = self.champ_list.index(event.widget)
+        b = self.champ_list_values[a]
+        if b:
+            self.champ_list_values[a] = False
+        else:
+            # now is checked
+            print(a)
+            self.champ_list_values[a] = True
+
+    def left_click_item(self, event):
+        # print(event.widget)
+        # print(self.item_dict)
+        a = self.item_list.index(event.widget)
+        b = self.item_list_values[a]
+        if b != 9:
+            self.item_list_values[a] = b + 1
+            self.item_list[a]["text"] = f"text: {b + 1}"
+        # a = self.label["text"]
+        # self.label['text'] = a - 1
+
+    def right_click_item(self, event):
+
+        a = self.item_list.index(event.widget)
+        b = self.item_list_values[a]
+        if b != 0:
+            self.item_list_values[a] = b - 1
+            self.item_list[a]["text"] = f"text: {b - 1}"
+
+    def res(self):
+        print(self.item_list_values)
+        print(self.champ_list_values)
 
 
 if __name__ == '__main__':
